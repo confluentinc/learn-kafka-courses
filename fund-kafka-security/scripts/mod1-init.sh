2012f08a20e9ffd426132ec5153f831f4f5afc7f
Dan Weston <dweston@confluent.io>
2022-05-31 17:14:21 -0700

    more edits

--- fund-kafka-security/scripts/mod1-init.sh
+++ fund-kafka-security/scripts/mod1-init.sh
@@ -4,10 +4,10 @@
 docker rm -f $(docker ps -aq) 1>/dev/null 2>&1
 docker system prune -f --volumes 1>/dev/null
 
-cd ~/tls
+cd ~/learn-kafka-courses/fund-kafka-security
 
 # Cleanup files
 find . \( -type f -name "*.key" -o -name "*.crt" -o -name "*.csr" -o -name "*.pem" -o -name "*.p12" -o -name "*.pkcs12" -o -name "*_creds" \)  -delete
 
 # Restore Module 1 Activity docker-compose.yml
-cp ~/tls/lab-initial-files/mod1-docker-compose.yml ~/tls/docker-compose.yml
+cp ~/learn-kafka-courses/fund-kafka-security/lab-initial-files/mod1-docker-compose.yml ~/learn-kafka-courses/fund-kafka-security/docker-compose.yml
