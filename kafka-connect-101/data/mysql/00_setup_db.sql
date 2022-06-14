6083fd35c5683218a31dda6736566f219df73cec
Dave Shook <45415767+daveshook@users.noreply.github.com>
2022-06-14 08:58:01 -0400

    Kafka connect 101 (#14)
    * kafka-connect-101 exercise files
    
    * added execute permission
    
    * added exercise specific docker-compose.yml files
    
    * added connector config json files for use with connect API and CLI
    
    * renamed files
    
    * corrected mysql-only docker configuration

---
+++ kafka-connect-101/data/mysql/00_setup_db.sql
@@ -0,0 +1,9 @@
+CREATE USER 'kc101user'@'%' IDENTIFIED WITH mysql_native_password BY 'kc101pw';
+CREATE USER 'replicator'@'%' IDENTIFIED BY 'replpass';
+
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'kc101user';
+GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator';
+
+create database demo;
+
+GRANT ALL PRIVILEGES ON demo.* TO 'kc101user'@'%';
