bd012b14582514c6a937eee7ec9c26a82e04d7e9
Dave Shook <dshook@confluent.io>
2022-08-04 20:50:08 -0400

    updated instructional text in producer

--- schema-registry/src/main/java/io/confluent/developer/ProducerApp.java
+++ schema-registry/src/main/java/io/confluent/developer/ProducerApp.java
@@ -34,10 +34,10 @@ public class ProducerApp {
 
         producerConfigs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, <Replace this with key serializer class>);
         producerConfigs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, <Replace this with value serializer class>);
-        // Setting auto-registration to false since we've already registered the schema manually
+        // Setting auto-registration to false since this is a best practice
         producerConfigs.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, false);
 
-        // Obvious duplication but this is done to emphasize what's needed to use SchemaRegistry
+        // Duplication of configs loaded from confluent.properties to emphasize what's needed to use SchemaRegistry
         producerConfigs.put("basic.auth.credentials.source", "USER_INFO");
         producerConfigs.put("schema.registry.url", "<Replace this with schema.registry.url value from confluent.properties>");
         producerConfigs.put("basic.auth.user.info", "<Replace this with basic.auth.user.info value from confluent.properties>");
