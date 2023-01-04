de9f0f1334b08dac015ba28989229f82da4292c5
Bill Bejeck <bbejeck@gmail.com>
2023-01-04 14:18:49 -0500

    Remove extra line

--- kafka-streams/src/main/java/io/confluent/developer/basic/BasicStreams.java
+++ kafka-streams/src/main/java/io/confluent/developer/basic/BasicStreams.java
@@ -1,6 +1,5 @@
 package io.confluent.developer.basic;
 
-
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
